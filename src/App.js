dcf18e89d15d401fc603a80f4b4858d583f6450b
Angelo Bennett <77983302+ahbenn86@users.noreply.github.com>
2022-04-21 21:56:17 -0400

    created favorites feature

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import "./App.css";
 import MovieList from "./components/MovieList";
 import MovieListHeading from "./components/MovieListHeading";
 import SearchBox from "./components/SearchBox";
+import AddFavorite from "./components/AddFavorite";
+import RemoveFavorite from "./components/RemoveFavorite";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+  const [favorites, setFavorites] = useState([]);
 
   const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=9f464a90`; 
-    
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=9f464a90`;
+
     const response = await fetch(url);
     const responseJson = await response.json();
 
@@ -24,7 +27,16 @@ function App() {
     getMovieRequest(searchValue);
   }, [searchValue]);
 
-  
+  const AddFavoriteMovie = (movie) => {
+    const newFavoriteList = [...favorites, movie];
+    setFavorites(newFavoriteList);
+  };
+
+  const RemoveFavoriteMovie = (movie) => {
+    const newFavoriteList = favorites.filter(
+      (facvorite) => favorites.imdbID !== movie.imdbID
+    );
+  }
 
   return (
     <div className="container-fluid red-curtain">
@@ -33,7 +45,21 @@ function App() {
         <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
       <div className="row">
-        <MovieList movies={movies} />
+        <MovieList
+          movies={movies}
+          handleFavoritesClick={AddFavoriteMovie}
+          favoriteComponent={AddFavorite}
+        />
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Favorites" />
+      </div>
+      <div className="row">
+        <MovieList
+          movies={favorites}
+          handleFavoritesClick={RemoveFavoriteMovie}
+          favoriteComponent={RemoveFavorite}
+        />
       </div>
     </div>
   );
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,21 @@
 const MovieList = (props) => {
-    return (
-        <>
-        {props.movies.map((movie, index) => (
-            <div className="image-container d-flex justify-content-start m3">
-                <img src={movie.Poster} alt="movie poster" />
-            </div>
-        ) )}
-        </>
-    )
-}
+  const FavoriteComponent = props.favoriteComponent;
+  return (
+    <>
+      {props.movies.map((movie, index) => (
+        <div className="image-container d-flex justify-content-start m3">
+          <img src={movie.Poster} alt="movie poster" />
 
-export default MovieList
+          <div
+            onClick={() => props.handleFavoritesClick(movie)}
+            className="overlay d-flex align-items-center justify-content-center"
+          >
+            <FavoriteComponent />
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default MovieList;
